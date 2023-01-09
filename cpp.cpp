7f5ab0cb26ccc1e9fc46cabcb13d68abf2c538a5
AlzyWelzy <welzyalzy@gmail.com>
2023-01-09 13:55:08 +0530

    commenting it

--- cpp.cpp
+++ cpp.cpp
@@ -1,44 +1,44 @@
-// Stacks
+// // Stacks
 
-// Create a stack that will have a list of strings strored onto it
+// // Create a stack that will have a list of strings strored onto it
 
-#include <iostream>
-#include <string>
-#include <stack>
+// #include <iostream>
+// #include <string>
+// #include <stack>
 
-using namespace std;
+// using namespace std;
 
-int main()
-{
-    stack<string> s;
-    s.push("Hello");
-    s.push("World");
-    s.push("!");
-    //print all the elements in the stack
+// int main()
+// {
+//     stack<string> s;
+//     s.push("Hello");
+//     s.push("World");
+//     s.push("!");
+//     //print all the elements in the stack
 
-    // cout << stack << endl;
-
-
-    cout << s.top() << endl;
-    s.pop();
-    cout << s.top() << endl;
-    s.pop();
-    cout << s.top() << endl;
-    s.pop();
+//     // cout << stack << endl;
 
 
-
-    // write a program that will print the memory that data types take up
-
-    cout << "Size of int: " << sizeof(int) << endl;
-    cout << "Size of float: " << sizeof(float) << endl;
-    cout << "Size of double: " << sizeof(double) << endl;
-    cout << "Size of char: " << sizeof(char) << endl;
-    cout << "Size of bool: " << sizeof(bool) << endl;
+//     cout << s.top() << endl;
+//     s.pop();
+//     cout << s.top() << endl;
+//     s.pop();
+//     cout << s.top() << endl;
+//     s.pop();
 
 
 
+//     // write a program that will print the memory that data types take up
+
+//     cout << "Size of int: " << sizeof(int) << endl;
+//     cout << "Size of float: " << sizeof(float) << endl;
+//     cout << "Size of double: " << sizeof(double) << endl;
+//     cout << "Size of char: " << sizeof(char) << endl;
+//     cout << "Size of bool: " << sizeof(bool) << endl;
 
 
-    return 0;
-}
+
+
+
+//     return 0;
+// }
